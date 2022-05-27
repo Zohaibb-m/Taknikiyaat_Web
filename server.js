33ca386d2a984fddc84b2491357cd66e5ef2128b
Zohaib Munir <zohaibmunir32@gmail.com>
2022-05-27 21:55:11 +0500

    Initial commit

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.use(express.json());
 app.use(cors());
 
 
+
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
+
 const PORT=process.env.PORT || 5000
 app.listen(PORT,()=>{
     console.log("Server Started at Port: ",PORT);
